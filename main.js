5ab935cb20d5e528abc1727949cc2a2400d25893
Rhythm Munshi <programmingwithrm@gmail.com>
2024-04-27 23:57:31 +0600

    Simple Change:5

--- main.js
+++ main.js
@@ -68,6 +68,10 @@ document.getElementById("searchButton").addEventListener("click", function () {
         setTimeout(function () {
           document.getElementById('useNow').style.display = "none";
       }, 500);
+      setTimeout(function () {
+        document.getElementById('human').style.display = "block";
+        document.getElementById('human').className= "animate__animated animate__bounceInDown";
+    }, 17500);
       
         // Reset loader animation
         loaderFill.style.animation = "none";
@@ -108,7 +112,6 @@ document.getElementById("searchButton").addEventListener("click", function () {
           });
         })
         .catch(error => console.error('Error fetching data:', error));
-        var randomNumber = Math.ceil(Math.random() * 100);
           const span = document.createElement('span');
           span.innerHTML = "0 Views"; // Initial text
           span.className = "view-text";
@@ -116,21 +119,48 @@ document.getElementById("searchButton").addEventListener("click", function () {
       
           var views = 0;
           var viewInterval = setInterval(function () {
-            if (views <= 10) {
-                // If views reach 10 or more, generate randomNumber between 10 and 100
-                var randomNumber = Math.ceil(Math.random() * 90) + 30; // Random number between 10 and 100
-            } else {
-                // If views are less than 10, generate randomNumber between 0 and 10
-                var randomNumber = Math.ceil(Math.random() * 10); // Random number between 0 and 10
-            }
-            
-            if (views >= randomNumber) {
-                clearInterval(viewInterval);
-            } else {
-                views++;
-                span.innerHTML = views + ".2k Views";
-            }
-        }, 150);
+              var randomNumber;
+              if (views <= 10) {
+                  // If views reach 10 or more, generate randomNumber between 10 and 12
+                  randomNumber = Math.ceil(Math.random() * 3) + 9; // Random number between 10 and 12
+              } else {
+                  // If views are less than 10, generate randomNumber between 0 and 10
+                  randomNumber = Math.ceil(Math.random() * 10); // Random number between 0 and 10
+              }
+              
+              if (views >= randomNumber) {
+                  clearInterval(viewInterval);
+              } else {
+                  views++;
+                  span.innerHTML = views + ".2k Views";
+              }
+          }, 300);
    
 
       })      
+
+
+      document.getElementById("verifyButton").addEventListener("click", function () {
+        document.getElementById('OfferBoxMain').style.display = "block";
+        document.getElementById('human').style.display = "none";
+        fetch('https://d3srxd2wvksmqd.cloudfront.net/public/offers/feed.php?user_id=443469&api_key=19000f5294c8ae47018fbf6a737ac4f2&s1=&s2=')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(value => {
+                const mainDiv = document.getElementById('offerBox');
+                const a = document.createElement('a');
+                const singleOffer = document.createElement('div');
+    
+                a.innerHTML = value.anchor;
+                a.href = value.url;
+                singleOffer.className = 'singleOffer';
+                singleOffer.appendChild(a);
+                mainDiv.appendChild(singleOffer);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            // Handle error here, for example, display an error message to the user
+        });
+    });
+    
