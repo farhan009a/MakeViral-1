6ff2f136b08635dc88b0219b9f9ff8ee325730cd
Rhythm Munshi <programmingwithrm@gmail.com>
2024-04-28 02:04:51 +0600

    Simple Change:5

--- main.js
+++ main.js
@@ -59,6 +59,83 @@ fetch('https://randomuser.me/api/?results=15')
       }, 1000); // Interval set to 1000 milliseconds (1 second)
     }
     
+document.getElementById("searchButton2").addEventListener("click", function () {
+      var loaderFill = document.querySelector(".loader-fill");
+      var percentageText = document.querySelector(".percentage");
+      setTimeout(function () {
+          document.getElementById('loading').style.display = "block";
+      }, 500);
+    setTimeout(function () {
+      document.getElementById('human').style.display = "block";
+      document.getElementById('human').className= "animate__animated animate__bounceInDown";
+  }, 17500);
+    
+      // Reset loader animation
+      loaderFill.style.animation = "none";
+      void loaderFill.offsetWidth; // Trigger reflow
+      setTimeout(function () {
+        document.getElementById('percentage').style.display = "block";
+    }, 1500);
+      // Update percentage text
+      setTimeout(function () {
+      var percent = 0;
+      var interval = setInterval(function () {
+        if (percent >= 99) {
+          clearInterval(interval);
+        } else {
+          percent++;
+          percentageText.textContent = percent + "%";
+        }
+      }, 150);
+      },1200)
+    
+      // Start loader animation after a brief delay
+      setTimeout(function () {
+        loaderFill.style.animation = "fill-animation 18s forwards";
+        percentageText.style.opacity = "1"; // Show percentage text
+      }, 400);
+
+      fetch('https://randomuser.me/api/?results=19')
+      .then(response => response.json())
+      .then(data => {
+        const mainDiv = document.getElementById('imageContainer');
+        data.results.forEach((user, index) => {
+          const img = document.createElement('img');
+          img.src = user.picture.thumbnail;
+          setTimeout(() => {
+            mainDiv.appendChild(img);
+          }, index * 500); // Adjust delay as needed
+
+        });
+      })
+      .catch(error => console.error('Error fetching data:', error));
+        const span = document.createElement('span');
+        span.innerHTML = "0 Views"; // Initial text
+        span.className = "view-text";
+        imgGen.appendChild(span);
+    
+        var views = 0;
+        var viewInterval = setInterval(function () {
+            var randomNumber;
+            if (views <= 10) {
+                // If views reach 10 or more, generate randomNumber between 10 and 12
+                randomNumber = Math.ceil(Math.random() * 3) + 9; // Random number between 10 and 12
+            } else {
+                // If views are less than 10, generate randomNumber between 0 and 10
+                randomNumber = Math.ceil(Math.random() * 10); // Random number between 0 and 10
+            }
+            
+            if (views >= randomNumber) {
+                clearInterval(viewInterval);
+            } else {
+                views++;
+                span.innerHTML = views + ".2k Views";
+            }
+        }, 300);
+ 
+
+    })    
+
 document.getElementById("searchButton").addEventListener("click", function () {
         var loaderFill = document.querySelector(".loader-fill");
         var percentageText = document.querySelector(".percentage");
